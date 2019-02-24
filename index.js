8e88099918721de47b60552c09e14468438db2e8
Ernie Val <erndval@gmail.com>
2019-02-24 19:50:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  var newObj = Object.assign({}, obj)
-  newObJ = 
+  var newObj = Object.assign({}, obj);
+  newObJ[key] = value;
+  return newObj;
 }
